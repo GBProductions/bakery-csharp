26c345155b61c303058c88d537d31affce475166
Garrett Brown <garrettpaulbrown@gmail.com>
2020-12-11 14:23:46 -0800

    Switch over to new file, might lose commits.

--- BakeryTests/ModelTests/BakeryTests.cs
+++ BakeryTests/ModelTests/BakeryTests.cs
@@ -15,11 +15,5 @@ namespace Bakery.Tests
             Assert.AreEqual(true, testBread.BreadCost(5));
         }
 
-        // [TestMethod]
-        // public void BakeryCost_BreadCostsFiveDollars_True()
-        // {
-        //     Bread testBread = new Bread();
-        //     Assert.AreEqual(true, testBread.BreadCost(5));
-        // }
     }
 }
